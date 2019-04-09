23740725f4850e68306e4765c3d49d2858e91e33
Stefan <smolerovic@groundlink.com>
2019-04-09 20:03:30 +0200

    Test

---
+++ precache-manifest.f61b494272e2673ef35b088ae4862116.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4270e0427f98c579fd69ae3dfd4f4f5",
+    "url": "/static/media/p6.b4270e04.jpg"
+  },
+  {
+    "revision": "923cdf217306b59f73f9",
+    "url": "/static/css/main.b06af71e.chunk.css"
+  },
+  {
+    "revision": "2449eaeb33a3104f6ed5cc39ce358625",
+    "url": "/static/media/section_two-min.2449eaeb.png"
+  },
+  {
+    "revision": "6fd3bb213887730e5dcd",
+    "url": "/static/js/1.6fd3bb21.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a1be69fbb0bcf156f7e7dca2b8d6b6f",
+    "url": "/static/media/kurs1-min.8a1be69f.jpg"
+  },
+  {
+    "revision": "f6bf086a4267a10499bb7dd2a1c42c49",
+    "url": "/static/media/linkedin-min.f6bf086a.png"
+  },
+  {
+    "revision": "fe097362b0bf655000a90380704b8ad5",
+    "url": "/static/media/krug-min.fe097362.png"
+  },
+  {
+    "revision": "ff49225935456a26d34542fa0fcee4a3",
+    "url": "/static/media/facebook-png-min.ff492259.png"
+  },
+  {
+    "revision": "eebb00a9bf839558b11fa817a62ddeac",
+    "url": "/static/media/kontex_logo-min.eebb00a9.png"
+  },
+  {
+    "revision": "6600c309845c364b846932c75f1e8a15",
+    "url": "/static/media/home_school_photo-min.6600c309.jpg"
+  },
+  {
+    "revision": "eae9651daa602adca08a9d1e344c858f",
+    "url": "/static/media/home_school_picoftheweek-min.eae9651d.png"
+  },
+  {
+    "revision": "87acb195e5ef06276c405219852d20d2",
+    "url": "/static/media/teaching-min.87acb195.png"
+  },
+  {
+    "revision": "dc0d6ac55f80c8bfe61bbf3675a180d9",
+    "url": "/static/media/kurs2-min.dc0d6ac5.jpg"
+  },
+  {
+    "revision": "e676bf493e3bb47ed5c0692e8b329c1d",
+    "url": "/static/media/kurs3-min.e676bf49.jpg"
+  },
+  {
+    "revision": "e2558ee7755793861011fece044279d0",
+    "url": "/static/media/p1.e2558ee7.jpg"
+  },
+  {
+    "revision": "56d34677250830f760426b0fccef3da6",
+    "url": "/static/media/p2.56d34677.jpg"
+  },
+  {
+    "revision": "363f2b3a6922d6de908f968c34f9406d",
+    "url": "/static/media/p3.363f2b3a.jpg"
+  },
+  {
+    "revision": "2f3e3da498a94f0b2daacd567917a011",
+    "url": "/static/media/p4.2f3e3da4.jpg"
+  },
+  {
+    "revision": "4131951884e7d14dd79592e439f44dfa",
+    "url": "/static/media/p5.41319518.jpg"
+  },
+  {
+    "revision": "923cdf217306b59f73f9",
+    "url": "/static/js/main.923cdf21.chunk.js"
+  },
+  {
+    "revision": "1a0170debd64850947cc150c78f58bbd",
+    "url": "/static/media/p7.1a0170de.jpg"
+  },
+  {
+    "revision": "c49fe82ccd167d9dc78825a4fd377ff1",
+    "url": "/static/media/p8.c49fe82c.jpg"
+  },
+  {
+    "revision": "9d22dbe70a109fdb46c15647aa198b97",
+    "url": "/static/media/p9.9d22dbe7.jpg"
+  },
+  {
+    "revision": "987bb11e15617dce5c9ed88f521f860c",
+    "url": "/static/media/p10.987bb11e.jpg"
+  },
+  {
+    "revision": "fa0ea3a0d3d73c0775b0e72a4088d2be",
+    "url": "/static/media/p11.fa0ea3a0.jpg"
+  },
+  {
+    "revision": "76b477bc42e0f1fc4dffe4c70ffd1eb0",
+    "url": "/static/media/p12.76b477bc.jpg"
+  },
+  {
+    "revision": "0f3ca5e1b5b5670d1b13ddb26088da29",
+    "url": "/static/media/p13.0f3ca5e1.jpg"
+  },
+  {
+    "revision": "952cc0b7f0d1a798fae3db447224351c",
+    "url": "/static/media/p14.952cc0b7.jpg"
+  },
+  {
+    "revision": "2e8ec4c0f654d81cdc8e74148b9228b5",
+    "url": "/static/media/p15.2e8ec4c0.jpg"
+  },
+  {
+    "revision": "861e2040ed6e425e55b9e0aa34efb186",
+    "url": "/static/media/magare-min.861e2040.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fbd5c3411627eb21f6306a7fc40b372b",
+    "url": "/static/media/home_school-min.fbd5c341.jpg"
+  },
+  {
+    "revision": "4b628a8d5ac789ec58565e0144b1f01b",
+    "url": "/static/media/section_one-min.4b628a8d.png"
+  },
+  {
+    "revision": "f51f43c883a15e1b164cff9c6f5fbf81",
+    "url": "/static/media/section_three-min.f51f43c8.png"
+  },
+  {
+    "revision": "6fd3bb213887730e5dcd",
+    "url": "/static/css/1.297fe9be.chunk.css"
+  },
+  {
+    "revision": "9c695f083d59966c7d49ae4712b834d9",
+    "url": "/index.html"
+  }
+];
