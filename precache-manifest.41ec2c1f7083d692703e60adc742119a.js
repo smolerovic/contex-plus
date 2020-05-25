5fb7ab87ecc0a4ebc808409153db92eef385fb98
Stefan <smolerovic@groundlink.com>
2020-05-25 14:40:50 +0200

    Updates

---
+++ precache-manifest.41ec2c1f7083d692703e60adc742119a.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e2e9536676c3740de9d5305cb65740",
+    "url": "/contex-plus/index.html"
+  },
+  {
+    "revision": "8ae811ba6019247b7259",
+    "url": "/contex-plus/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "f54b7d84786e0aa262b9",
+    "url": "/contex-plus/static/css/main.9f2ff66e.chunk.css"
+  },
+  {
+    "revision": "8ae811ba6019247b7259",
+    "url": "/contex-plus/static/js/2.cc306ba7.chunk.js"
+  },
+  {
+    "revision": "f54b7d84786e0aa262b9",
+    "url": "/contex-plus/static/js/main.1631992e.chunk.js"
+  },
+  {
+    "revision": "67e4bafab92dbc48b101",
+    "url": "/contex-plus/static/js/runtime~main.a816323a.js"
+  },
+  {
+    "revision": "60b0d49aa648eebacb1885cbc510da3f",
+    "url": "/contex-plus/static/media/cenovnik-za-kurseve-engleskog-jezika.60b0d49a.pdf"
+  },
+  {
+    "revision": "31afeac2a734275c5c7a77d7189e598c",
+    "url": "/contex-plus/static/media/cenovnik-za-kurseve-italijanskog-jezika.31afeac2.pdf"
+  },
+  {
+    "revision": "293c5dfe0cc9bf3663f3a7bf0e7f7e56",
+    "url": "/contex-plus/static/media/cenovnik-za-kurseve-nemackog-jezika.293c5dfe.pdf"
+  },
+  {
+    "revision": "ff49225935456a26d34542fa0fcee4a3",
+    "url": "/contex-plus/static/media/facebook-png-min.ff492259.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contex-plus/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contex-plus/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contex-plus/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contex-plus/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contex-plus/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fbd5c3411627eb21f6306a7fc40b372b",
+    "url": "/contex-plus/static/media/home_school-min.fbd5c341.jpg"
+  },
+  {
+    "revision": "6600c309845c364b846932c75f1e8a15",
+    "url": "/contex-plus/static/media/home_school_photo-min.6600c309.jpg"
+  },
+  {
+    "revision": "eae9651daa602adca08a9d1e344c858f",
+    "url": "/contex-plus/static/media/home_school_picoftheweek-min.eae9651d.png"
+  },
+  {
+    "revision": "03cc96edd188896c4b8b1b2fe8d0c252",
+    "url": "/contex-plus/static/media/kontex_logo-min.03cc96ed.png"
+  },
+  {
+    "revision": "fe097362b0bf655000a90380704b8ad5",
+    "url": "/contex-plus/static/media/krug-min.fe097362.png"
+  },
+  {
+    "revision": "8a1be69fbb0bcf156f7e7dca2b8d6b6f",
+    "url": "/contex-plus/static/media/kurs1-min.8a1be69f.jpg"
+  },
+  {
+    "revision": "dc0d6ac55f80c8bfe61bbf3675a180d9",
+    "url": "/contex-plus/static/media/kurs2-min.dc0d6ac5.jpg"
+  },
+  {
+    "revision": "e676bf493e3bb47ed5c0692e8b329c1d",
+    "url": "/contex-plus/static/media/kurs3-min.e676bf49.jpg"
+  },
+  {
+    "revision": "865f3bfbd6804fdd8b139c2ea5babe4f",
+    "url": "/contex-plus/static/media/kurs4-min.865f3bfb.jpg"
+  },
+  {
+    "revision": "43efe09fff338a4f528069916c1c84c8",
+    "url": "/contex-plus/static/media/kurs5-min.43efe09f.jpg"
+  },
+  {
+    "revision": "f6bf086a4267a10499bb7dd2a1c42c49",
+    "url": "/contex-plus/static/media/linkedin-min.f6bf086a.png"
+  },
+  {
+    "revision": "861e2040ed6e425e55b9e0aa34efb186",
+    "url": "/contex-plus/static/media/magare-min.861e2040.png"
+  },
+  {
+    "revision": "78267972c65c0e512f19b91b3ffc326d",
+    "url": "/contex-plus/static/media/otkupantikviteta.78267972.gif"
+  },
+  {
+    "revision": "e2558ee7755793861011fece044279d0",
+    "url": "/contex-plus/static/media/p1.e2558ee7.jpg"
+  },
+  {
+    "revision": "987bb11e15617dce5c9ed88f521f860c",
+    "url": "/contex-plus/static/media/p10.987bb11e.jpg"
+  },
+  {
+    "revision": "fa0ea3a0d3d73c0775b0e72a4088d2be",
+    "url": "/contex-plus/static/media/p11.fa0ea3a0.jpg"
+  },
+  {
+    "revision": "76b477bc42e0f1fc4dffe4c70ffd1eb0",
+    "url": "/contex-plus/static/media/p12.76b477bc.jpg"
+  },
+  {
+    "revision": "0f3ca5e1b5b5670d1b13ddb26088da29",
+    "url": "/contex-plus/static/media/p13.0f3ca5e1.jpg"
+  },
+  {
+    "revision": "952cc0b7f0d1a798fae3db447224351c",
+    "url": "/contex-plus/static/media/p14.952cc0b7.jpg"
+  },
+  {
+    "revision": "2e8ec4c0f654d81cdc8e74148b9228b5",
+    "url": "/contex-plus/static/media/p15.2e8ec4c0.jpg"
+  },
+  {
+    "revision": "56d34677250830f760426b0fccef3da6",
+    "url": "/contex-plus/static/media/p2.56d34677.jpg"
+  },
+  {
+    "revision": "363f2b3a6922d6de908f968c34f9406d",
+    "url": "/contex-plus/static/media/p3.363f2b3a.jpg"
+  },
+  {
+    "revision": "2f3e3da498a94f0b2daacd567917a011",
+    "url": "/contex-plus/static/media/p4.2f3e3da4.jpg"
+  },
+  {
+    "revision": "4131951884e7d14dd79592e439f44dfa",
+    "url": "/contex-plus/static/media/p5.41319518.jpg"
+  },
+  {
+    "revision": "b4270e0427f98c579fd69ae3dfd4f4f5",
+    "url": "/contex-plus/static/media/p6.b4270e04.jpg"
+  },
+  {
+    "revision": "1a0170debd64850947cc150c78f58bbd",
+    "url": "/contex-plus/static/media/p7.1a0170de.jpg"
+  },
+  {
+    "revision": "c49fe82ccd167d9dc78825a4fd377ff1",
+    "url": "/contex-plus/static/media/p8.c49fe82c.jpg"
+  },
+  {
+    "revision": "9d22dbe70a109fdb46c15647aa198b97",
+    "url": "/contex-plus/static/media/p9.9d22dbe7.jpg"
+  },
+  {
+    "revision": "4b628a8d5ac789ec58565e0144b1f01b",
+    "url": "/contex-plus/static/media/section_one-min.4b628a8d.png"
+  },
+  {
+    "revision": "f51f43c883a15e1b164cff9c6f5fbf81",
+    "url": "/contex-plus/static/media/section_three-min.f51f43c8.png"
+  },
+  {
+    "revision": "2449eaeb33a3104f6ed5cc39ce358625",
+    "url": "/contex-plus/static/media/section_two-min.2449eaeb.png"
+  },
+  {
+    "revision": "230671e1f5ad9c235cde13931c0a650e",
+    "url": "/contex-plus/static/media/teaching-min.230671e1.png"
+  }
+]);
